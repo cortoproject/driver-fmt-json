9735e5005e8ecbc67d3bee8b2bac003f5cb4101a
Sander Mertens <sander.mertens8@gmail.com>
2016-04-19 02:37:20 -0400

    Change "" to <>

--- test/include/test.h
+++ test/include/test.h
@@ -6,11 +6,11 @@
 #ifndef TEST_H
 #define TEST_H
 
-#include "corto/corto.h"
-#include "_type.h"
-#include "_api.h"
-#include "_meta.h"
-#include "_interface.h"
+#include <corto/corto.h>
+#include <_type.h>
+#include <_api.h>
+#include <_meta.h>
+#include <_interface.h>
 
 #include "corto/fmt/json/json.h"
 #include "corto/test/test.h"
@@ -19,11 +19,11 @@
 extern "C" {
 #endif
 
-#include "Line.h"
-#include "Point.h"
-#include "Point3D.h"
-#include "ReferenceType.h"
-#include "Serializer.h"
+#include <Line.h>
+#include <Point.h>
+#include <Point3D.h>
+#include <ReferenceType.h>
+#include <Serializer.h>
 
 #ifdef __cplusplus
 }
--- test/src/Serializer.c
+++ test/src/Serializer.c
@@ -6,7 +6,7 @@
  * when the file is regenerated.
  */
 
-#include "test.h"
+#include <test.h>
 
 corto_void _test_Serializer_tc_serArray(
     test_Serializer this)
